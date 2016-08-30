3efe89e53f8a291d2de02ca3e5bc576306374fa3
ItsDavidThai <itsdavidthai@gmail.com>
2016-08-29 21:28:33 -0700

    added some styling

--- components/App.jsx
+++ components/App.jsx
@@ -1,16 +1,24 @@
 var React = require('react')
-
+var ReactBootstrap = require('react-bootstrap')
+var Jumbotron = ReactBootstrap.Jumbotron
+var Button = ReactBootstrap.Button
 var QuizBody = (props) =>{
   return(
+
+
     <div>
       {props.data.map( (element) => ( <QuizQuestion data={element} />) )}
     </div>
   )
 
+
 }
+
+
 
 var QuizQuestion = (props) => {
   return(
+
       <div>{props.data}</div>
     )
 }
@@ -23,7 +31,12 @@ class App extends React.Component {
   render(){
     return(
         <div>
+         <Jumbotron>
+            <h1>Quizzer</h1>
+            <p>Are you ready for some quizzes?! great!------</p>
+            <p><Button bsStyle="primary">Learn more</Button></p>
           <QuizBody data={this.props.data}/>
+          </Jumbotron>
         </div>
     )
   }
--- server.js
+++ server.js
@@ -3,7 +3,9 @@ var app = express();
 var React = require('react');
 var ReactDOMServer = require('react-dom/server')
 var App = require('./components/App.jsx')
-var _ = require('underscore')
+
+
+// var _ = require('underscore')
 
 // app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
@@ -11,7 +13,10 @@ app.set('view engine', 'ejs');
 app.get('/', function(req, res){
   // var html = _.template(template, data);
   console.log('request to root')
-  var html = ReactDOMServer.renderToString(<App data={[1,2,3]}/>)
+  console.log(html)
+  var html = '<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/latest/css/bootstrap.min.css">'
+
+  + ReactDOMServer.renderToString(<App data={[1,2,3,4]}/>)
   console.log(html)
   res.send(html)
 
